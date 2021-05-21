162980bb3707c8fd638f1d4def9fbb739645dace
MirrorChu <aikgcfhl@gmail.com>
2021-05-21 07:39:06 -0700

    Update MyThriftConnector.java

--- plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/MyThriftConnector.java
+++ plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/MyThriftConnector.java
@@ -10,8 +10,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * @author XU Boqing
- * @version 1.3.0
  */
 package io.trino.plugin.iceberg;
 
